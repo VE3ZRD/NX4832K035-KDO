7d2d107ec36f458d24b58dcefcafeaf2da36da5a
VE3ZRD <ve3zrd@gmail.com>
2024-06-03 21:42:13 -0400

    update

---
+++ lh2.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+#################################################
+#  Get Last Heard Liost from MMDVMHost Log	#
+#						#
+#						#
+#  VE3RD 			2020-05-03	#
+#################################################
+set -o errexit
+set -o pipefail
+sudo mount -o remount,rw /
+
+if [ -f /home/pi-star/lh2_start.txt ]; then
+  rm /home/pi-star/lh2_start.txt
+fi
+if [ -f /home/pi-star/lhlog.txt ]; then
+  rm /home/pi-star/lhlog.txt
+fi
+
+echo "Args = $@" >> /home/pi-star/lh2_start.txt
+
+name=""
+
+declare -i n
+####################################################
+function domode2
+{
+line3=""
+call1=""
+name=""
+while read -r line
+do
+	call=$(echo "$line" | cut -d' ' -f3)
+	tm=$(echo "$line" | cut -d' ' -f2)
+	dt=$(echo "$line" | cut -d' ' -f1)
+	pl=$(echo "$line" | cut -d' ' -f5)
+
+	echo "Add Call: $call" >> /home/pi-star/lh2_start.txt
+	dataline=$(sudo sed -n "/$call/p" /usr/local/etc/stripped.csv)
+       	did=$(echo "$dataline" | cut -d',' -f1 | head -1)
+        call1=$(echo "$dataline" | cut -d',' -f2 | head -1)
+
+        name=$(echo "$dataline" | cut -d',' -f3 | head -1)
+        city=$(echo "$dataline" | cut -d',' -f4 | head -1)
+        prov=$(echo "$dataline" | cut -d',' -f5 | head -1)
+        country=$(echo "$dataline" | cut -d',' -f7 | head -1)
+
+	if [ -z "$name" ]; then
+		name="Name N/A"
+	fi
+
+
+#	line3=$(echo  "$dt" "$tm" "$call" "$name" | awk '{printf  "%5s %s %s %s|\n", $1 $2 $3 $name}')
+	line3=""
+	echo  "$dt $tm $call $name |" |  tr -d "\n"
+#	echo "$line2"
+#	line4=${line3:0:37}
+#	line6=$(echo "$line3" | tr -d "\n")
+
+#	list5+="$line6"
+done <<< "$list9"
+
+var="${list5:0:400}"
+#echo "${var}"
+echo "${var}" >> ./lh2_start.txt
+
+}
+
+##########################################################
+function domode3
+{
+line3=""
+while read -r line
+do
+	call=$(echo "$line" | cut -d' ' -f3)
+	tm=$(echo "$line" | cut -d' ' -f2)
+	pl=$(echo "$line" | cut -d' ' -f5)
+
+	echo "Add Call: $call" >> /home/pi-star/lh2_start.txt
+	dataline=$(sudo sed -n "/$call/p" /usr/local/etc/stripped.csv)
+       	did=$(echo "$dataline" | cut -d',' -f1 | head -1)
+        name=$(echo "$dataline" | cut -d',' -f3 | head -1)
+        city=$(echo "$dataline" | cut -d',' -f4 | head -1)
+        prov=$(echo "$dataline" | cut -d',' -f5 | head -1)
+        country=$(echo "$dataline" | cut -d',' -f7 | head -1)
+if [ -z "$city" ]; then
+	city="N/A"
+fi
+	line3=$(echo "$call" "$name" "$city" "$prov" "$country"| awk '{printf  "%5s %s %s %s %s |\n", $1, $name, $3, $4, $5}'  |  tr -d "\n" )
+	echo "$call" "$name" "$city" "$prov" "$country|" | tr -d "\n" 
+#	line4=${line3:0:37}
+	line6=$(echo "$line3" | tr -d "\n")
+
+	list5+="$line6"
+done <<< "$list9"
+var="${list5:0:400}"
+echo "${var}"
+echo "${var}" >> ./lh2_start.txt
+
+#echo "$list3"
+}
+##################################################
+function domode4
+{
+line3=""
+while read -r line
+do
+	call=$(echo "$line" | cut -d' ' -f3)
+	tm=$(echo "$line" | cut -d' ' -f2)
+	pl=$(echo "$line" | cut -d' ' -f5)
+
+#	echo "Add Call: $call" >> /home/pi-star/lh2_start.txt
+	dataline=$(sudo sed -n "/$call/p" /usr/local/etc/stripped.csv)
+        name=$(echo "$dataline" | cut -d',' -f3 | head -1)
+       	did=$(echo "$dataline" | cut -d',' -f1 | head -1)
+
+	line2=$(echo "$line $name $did" | awk '{printf  "%5s %.5s %6s %7s %6s %6s|\n",$1,$2,$3,$6,$4,$7}')
+#	echo "  $line2"
+#	line3=${line2:0:37}
+	list4+="$line2"
+done <<< "$list9"
+var="${list4:0:400}"
+echo "${var}"
+echo "${var}" >> ./lh2_start.txt
+
+#echo "$list3"
+exit
+}
+###################################################
+
+
+######################################
+#Start of Main Program
+######################################
+
+f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -1)
+list1=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | sed 's/,//g' | sort -r -k14,14 -k2,3)
+
+f2=$(ls -tr /var/log/pi-star/MMDVM* | head -1)
+list2=$(sudo sed -n '/received network end of voice transmission from/p' $f2 | sed 's/,//g' | sort -r -k14,14 -k2,3)
+#echo "$list1"
+list3=$(echo "$list1" | awk '$14!=savestr {print substr($2,6,5),substr($3,0,6),$14,$17,$6,$18,$20; savestr=$14}' | sort -r -k1,2)
+list4=$(echo "$list2" | awk '$14!=savestr {print substr($2,6,5),substr($3,0,6),$14,$17,$6,$18,$20; savestr=$14}' | sort -r -k1,2)
+
+
+lcnt=$(echo "$list3" | wc -l)
+if [ $lcnt -lt 20 ]; then
+	list3+=$list4
+fi
+#  echo "Test1234 | Test 2345 | TestTest\r |12345678"
+
+#echo "$list3"
+
+######  Mode 1
+if [ "$2" == "1" ]; then
+	echo "Check OK 1 = $2" >> /home/pi-star/lh2_start.txt 
+
+	if [ "$1" == "1" ]; then
+		echo "$list3" | sed -n '1,7p;8q' |  awk '{printf "%5s %5s %7s %6s %1s %4s %-4s|\n", $1, $2, $3, $4, $5, $6, $7}' | tr -d "\n"
+	fi
+	if [ "$1" == "2" ]; then
+		echo "$list3" | sed -n '7,12p;13q' |  awk '{printf "%5s %5s %6s %6s %1s %4s %-4s|\n", $1, $2, $3, $4, $5, $6, $7}' | tr -d "\n"
+	fi
+	if [ "$1" == "3" ]; then
+		echo "$list3" | sed -n '13,18p;19q' |  awk '{printf "%5s %5s %6s %6s %1s %4s %-4s|\n", $1, $2, $3, $4, $5, $6, $7}' | tr -d "\n"
+	fi
+	if [ "$1" == "4" ]; then
+		echo "$list3" | sed -n '19,24p;25q' |  awk '{printf "%5s %5s %6s %6s %1s %4s %-4s|\n", $1, $2, $3, $4, $5, $6, $7}' | tr -d "\n"
+	fi
+fi
+######  Mode 2/3/4
+
+
+	if [ "$1" == "1" ]; then
+		list9=$(echo "$list3" | sed -n '1,8p;9q' | awk '{print $1, $2, $3, $4}' )
+	fi
+	if [ "$1" == "2" ]; then
+		list9=$(echo "$list3" | sed -n '7,12p;13q' | awk '{print $1, $2, $3, $4}') 
+	fi
+	if [ "$1" == "3" ]; then
+		list9=$(echo "$list3" | sed -n '13,18p;19q' | awk '{print $1, $2, $3, $4}') 
+	fi
+	if [ "$1" == "4" ]; then
+		list9=$(echo "$list3" | sed -n '19,24p;25q' | awk '{print $1, $2, $3, $4}') 
+	fi
+
+
+if [ "$2" == "2" ]; then
+                echo "Check OK 2 = $2" >> /home/pi-star/lh2_start.txt
+	domode2
+fi
+#####  Mode 3
+if [ "$2" == "3" ]; then
+        echo "Check OK 3 = $2" >> /home/pi-star/lh2_start.txt
+
+	domode3
+fi
+
+
+if [ "$2" == "4" ]; then
+        echo "Check OK 3 = $2" >> /home/pi-star/lh2_start.txt
+## No mode 4 yet, use mode 3
+	domode3
+
+fi
+
+sudo mount -o remount,ro /
